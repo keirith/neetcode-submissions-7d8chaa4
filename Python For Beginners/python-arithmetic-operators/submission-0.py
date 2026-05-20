020deee67fb5a898f3fc7b44480a7070491b08c2
Keirith <katwal95@gmail.com>
2026-05-20 10:51:07 -0700

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,13 @@
+a, b, c = 2, 2, 0.5
+
+#sum of values
+print(a+b+c)
+
+#0 minus sum of values
+print(0 - (a+b+c))
+
+#product of values
+print(a*b*c)
+
+#sum of values divided by product of values
+print((a+b+c)/(a*b*c))
