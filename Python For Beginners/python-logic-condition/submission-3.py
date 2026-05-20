7f5c52ea6aa3fe7c0295b1875a563eb020328945
Keirith <katwal95@gmail.com>
2026-05-20 12:19:44 -0700

    Add: python-logic-condition - submission-3

---
+++ Python For Beginners/python-logic-condition/submission-3.py
@@ -0,0 +1,19 @@
+def discount_applies(age: int) -> bool:
+    return age < 18 or age >=65
+
+    #long hand
+    # if age < 18 or age >= 65:
+    #     return True
+    # else:
+    #     return False
+
+
+
+
+
+# don't modify this code below this line
+print(discount_applies(17))
+print(discount_applies(18))
+print(discount_applies(40))
+print(discount_applies(65))
+print(discount_applies(70))
