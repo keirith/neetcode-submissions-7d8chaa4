f854df52da4d9d09052156fc4cb9ac79edc45541
Keirith <katwal95@gmail.com>
2026-05-21 11:38:16 -0700

    Add: python-strings-are-immutable - submission-0

---
+++ Python For Beginners/python-strings-are-immutable/submission-0.py
@@ -0,0 +1,11 @@
+def remove_fourth_character(word: str) -> str:
+    #Strings are immutable in python, so we must use splicing to create a new string.
+    before_fourth = word[:3]
+    after_fourth = word[4:]
+    
+    return before_fourth + after_fourth
+
+
+# do not modify below this line
+print(remove_fourth_character("NeetCode"))
+print(remove_fourth_character("Hello"))
