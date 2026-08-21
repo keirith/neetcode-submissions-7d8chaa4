08e97d8a0e2f939709d770b98aaad7538ec6c061
Keirith <katwal95@gmail.com>
2026-08-21 14:15:44 -0700

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,21 @@
+import heapq
+import math
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        '''
+        I want to use a heap to store a tuple leading with the euclid distances, then the cordinates of each point.
+        I'll want to use a max heap to keep up to k cordinates, so the largest euclid distance gets evicted.
+        '''
+        my_heap = []
+
+        for point in points:
+            e_distance = math.sqrt(((0 - point[0])**2 + (0 - point[1])**2))
+            heapq.heappush(my_heap, (-1 * e_distance, point))
+            if len(my_heap) > k:
+                heapq.heappop(my_heap)
+
+        return [ h[1] for h in my_heap ]
+        #Time: O(n log k)  n = len(points), k = restriction of number of points interested in.
+        #space: O(k) , at most k elements placed on heap.
+        
